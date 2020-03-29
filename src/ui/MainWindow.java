2a7301722a115c47b3de940613239130de402d60
Stefan <cveticstefancpl@gmail.com>
2020-03-29 15:52:50 +0200

    Refactor UI component names

--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -6,16 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class MainWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent tabPane = FXMLLoader.load(getClass().getResource("xml/window_main_layout.fxml"));
+        primaryStage.setTitle("Word Distribution Tool");
+        primaryStage.setScene(new Scene(tabPane, 1280, 720));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
