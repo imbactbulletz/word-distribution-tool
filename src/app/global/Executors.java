bdfa8f7d9316521747177da8a30f7b1670a14f97
Stefan <cveticstefancpl@gmail.com>
2020-04-05 01:48:50 +0200

    Add shared executor service for cruncher components

--- src/app/global/Executors.java
+++ src/app/global/Executors.java
@@ -7,9 +7,12 @@ public class Executors {
     static {
         COMPONENT = java.util.concurrent.Executors.newCachedThreadPool();
         INPUT = java.util.concurrent.Executors.newCachedThreadPool();
+        CRUNCHER = java.util.concurrent.Executors.newCachedThreadPool();
     }
 
     public static final ExecutorService COMPONENT;
 
     public static final ExecutorService INPUT;
+
+    public static final ExecutorService CRUNCHER;
 }
